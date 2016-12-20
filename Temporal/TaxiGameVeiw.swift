424762ffdd4d77b2f88abdbcb1eeae39abcc54cc
Milad Masoodi (student LM) <s017363@lm-18s017363.local>
2016-12-20 09:39:59 -0500

    dank 1

--- Temporal/TaxiGameVeiw.swift
+++ Temporal/TaxiGameVeiw.swift
@@ -17,7 +17,7 @@ class TaxiGameVeiw: UIViewController{
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = SKScene(fileNamed: "MyScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
